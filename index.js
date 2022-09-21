c12518d5df992882f39d0b9f6afdfde4bb9e260c
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-09-21 19:24:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,19 @@
 var Game = {
     canvas : undefined,
     canvasContext : undefined,
-    backgroundSprite : undefined,
-    balloonSprite : undefined,
-    balloonPosition : { x : 0, y : 50 }
+    rectanglePosition : 0
 };
 
 Game.start = function () {
     Game.canvas = document.getElementById("myCanvas");
     Game.canvasContext = Game.canvas.getContext("2d");
-    Game.backgroundSprite = new Image();
-    Game.backgroundSprite.src = "spr_background.jpg";
-    Game.balloonSprite = new Image();
-    Game.balloonSprite.src = "spr_balloon.png";
-    window.setTimeout(Game.mainLoop, 500);
+    Game.mainLoop();
 };
 
 document.addEventListener( 'DOMContentLoaded', Game.start);
 
 Game.clearCanvas = function () {
     Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
-};
-
-Game.drawImage = function (sprite, position) {
-    Game.canvasContext.save();
-    Game.canvasContext.translate(position.x, position.y);
-    Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height,
-        0, 0, sprite.width, sprite.height);
-    Game.canvasContext.restore();
 };
 
 Game.mainLoop = function() {
@@ -41,10 +27,10 @@ Game.mainLoop = function() {
 
 Game.update = function () {
     var d = new Date();
-    Game.balloonPosition.x = d.getTime() * 0.3 % Game.canvas.width;
+    Game.rectanglePosition = d.getTime() % Game.canvas.width;
 };
 
 Game.draw = function () {
-    Game.drawImage(Game.backgroundSprite, { x : 0, y : 0 });
-    Game.drawImage(Game.balloonSprite, Game.balloonPosition);
+    Game.canvasContext.fillStyle = "blue";
+    Game.canvasContext.fillRect(Game.rectanglePosition, 100, 50, 50);
 };
