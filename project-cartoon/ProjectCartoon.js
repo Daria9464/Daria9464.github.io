70d9078a51768fad89d8548ce893d49a2b6042b3
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-09-27 00:58:45 -0400

    Update ProjectCartoon.js

--- project-cartoon/ProjectCartoon.js
+++ project-cartoon/ProjectCartoon.js
@@ -1,85 +1,86 @@
-window.onload= function(){
-    var canvas = document.getElementById("canvas");
-    canvas.width=800;
-    canvas.height=600;
-    var context = canvas.getContext("2d");
+window.onload = function(){
+     var cvs = document.getElementById('canvas');
+     var cxt = cvs.getContext('2d');
+     var R;
+ 
+     //draw stars
+     for(var i=0;i<100;i++){
+         R = 2+5*Math.random();
+         drawStar(cxt,cvs.width*Math.random(),cvs.height*0.7*Math.random(),R,'#FFFF00');
+     }
+ 
+     //draw moon
+     drawMoon(cxt,600,100,40,3,30,'#FFF68F')
+ 
+     //draw grassGround
+     cxt.save();
+     cxt.beginPath();
+     cxt.moveTo(0,400);
+     cxt.bezierCurveTo(200,300,500,500,800,450);
+     cxt.lineTo(800,600);
+     cxt.lineTo(0,800);
+     cxt.closePath();
+     cxt.fillStyle = "DarkGreen";
+     cxt.fill();
+     cxt.restore(); 
+     
 
-    //draw stars 
-    for(var i=0 ;i<200;i++){
-        var R = Math.random()*10;
-        var x = Math.random()*canvas.width;
-        var y = Math.random()*400;
-        var r = Math.random()*360;
-        drawStar(context,R/2,R,x,y,r);
-    }
-}
-function drawStar(cxt, r,R ,x,y,rota){
-    cxt.beginPath();
-    for(var i =0 ;i<5 ;i++){
-        cxt.lineTo(Math.cos((18+i*72-rota)/180*Math.PI)*R+x
-        ,-Math.sin((18+i*72-rota)/180*Math.PI)*R+y);
-        
-        cxt.lineTo(Math.cos((54+i*72-rota)/180*Math.PI)*r+x
-        ,-Math.sin((54+i*72-rota)/180*Math.PI)*r+y);
-    }
-    cxt.closePath(); 
-    
-    cxt.fillStyle="yellow";
-    cxt.strokeStyle="yellow";
-    cxt.lineWidth=3;
-    cxt.lineJoin="round";
-    cxt.fill();
-    cxt.stroke();
-    cxt.restore();
-    
-//draw moon
-drawMoon(cxt,600,100,40,3,30,'#FFF68F');    
-function drawMoon(cxt,x,y,r,d,rot,fillColor){
+//draw a house  with a chimney
+     cxt.save();
+     cxt.beginPath();
+     cxt.fillStyle="CornflowerBlue";
+     cxt.strokeStyle="Black";
+     cxt.fillRect(100,400,100,100);
+     cxt.strokeRect(100,400,100,100);
+     cxt.fillStyle="White";
+     cxt.strokeStyle="Black";
+     cxt.fillRect(165,330,10,45);
+     cxt.strokeRect(165,330,10,45);
+     cxt.moveTo(100,400);
+     cxt.lineTo(150,350);
+     cxt.lineTo(200,400);
+     cxt.closePath();
+     cxt.fillStyle = "Crimson";
+     cxt.fill();
+     cxt.stroke();
+     cxt.restore();
+
+ }
+ 
+ function drawMoon(cxt,x,y,r,d,rot,fillColor){
      cxt.save();
      cxt.translate(x,y);
      cxt.scale(r,r);
      cxt.rotate(rot*Math.PI/180);
      moonPath(cxt,d);
-     cxt.fillStyle ="yellow";
+     cxt.fillStyle = 'yellow' || fillColor;
      cxt.fill();
      cxt.restore();
  }
-function moonPath(cxt,d){
+ 
+ function moonPath(cxt,d){
      cxt.beginPath();
      cxt.arc(0,0,1,0.5*Math.PI,1.5*Math.PI,true);
      cxt.moveTo(0,-1);
      cxt.arcTo(d,0,0,1,Math.sqrt(1+d*d)/d);
- }   
-    
-//draw ground
-cxt.save();
-cxt.beginPath();
-cxt.moveTo(0,400);
-cxt.bezierCurveTo(200,300,500,500,800,450);
-cxt.lineTo(800,600);
-cxt.lineTo(0,800);
-cxt.closePath();
-cxt.fillStyle ="green";
-cxt.fill(); 
-
-//draw a chimney of the house
-cxt.save();
-ctx.fillStyle = "Grey";
-ctx.strokeStyle ="black";
-ctx.fillRect(665, 330,10, 45);
-ctx.strokeRect(665, 330,10, 45);
-
-//draw a house
-ctx.fillStyle = "CornflowerBlue";
-ctx.strokeStyle ="black";
-ctx.fillRect(600, 400,100, 100);
-ctx.strokeRect(600, 400,100, 100);
-ctx.beginPath();
-ctx.fillStyle="Crimson"
-ctx.moveTo(600, 400);
-ctx.lineTo(650, 350);
-ctx.lineTo(700, 400);
-ctx.fill();
-ctx.stroke();
-cxt.closePath();
-}
+ }
+ 
+ function drawStar(cxt,x,y,R,fillColor){
+     cxt.save();
+     cxt.translate(x,y);
+     cxt.rotate(360*Math.random()*Math.PI/180);
+     cxt.scale(R,R);
+     starPath(cxt);
+     cxt.fillStyle = fillColor;
+     cxt.fill();
+     cxt.restore();
+ 
+ }
+ function starPath(cxt){
+     cxt.beginPath();
+     for(var i=0;i<5;i++){
+         cxt.lineTo(Math.cos((18+72*i)*Math.PI/180),-Math.sin((18+72*i)*Math.PI/180));
+         cxt.lineTo(Math.cos((54+72*i)*Math.PI/180)*0.5,-Math.sin((54+72*i)*Math.PI/180)*0.5);
+     }
+     cxt.closePath();
+ }
