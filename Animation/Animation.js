b8ecb1074e8a2b17d14406b285a15f4fa5a5550b
Daria9464 <danying2432@gmail.com>
2022-10-12 17:38:10 -0400

    update

---
+++ Animation/Animation.js
@@ -0,0 +1,6 @@
+var scene = [
+    {picPage:"1st", text:"Hello! My name is Danying Chen. How are you today?"},
+    {picPage:"2nd", text:"Nice to meet you!"},
+    {picPage:"3rd", text:"Witter is coming."},
+    {picPage:"4th", text:"Wear sweather to keep warm!"},
+    ];
