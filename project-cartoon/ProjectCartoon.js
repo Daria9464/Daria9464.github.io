92f5647523533fb68b5ab08d9b681ce7cd8907a3
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-10-12 19:34:03 -0400

    Update ProjectCartoon.js

--- project-cartoon/ProjectCartoon.js
+++ project-cartoon/ProjectCartoon.js
@@ -26,37 +26,7 @@ window.onload = function(){
      cxt.fill();
      cxt.restore(); 
      
-    //draw stars 
-    for(var i=0 ;i<200;i++){
-        var R = Math.random()*10;
-        var x = Math.random()*canvas.width;
-        var y = Math.random()*400;
-        var r = Math.random()*360;
-        drawStar(context,R/2,R,x,y,r);
-    }
-}
-function drawStar(cxt, r,R ,x,y,rota){
-    cxt.beginPath();
-    for(var i =0 ;i<5 ;i++){
-        cxt.lineTo(Math.cos((18+i*72-rota)/180*Math.PI)*R+x
-        ,-Math.sin((18+i*72-rota)/180*Math.PI)*R+y);
-        
-        cxt.lineTo(Math.cos((54+i*72-rota)/180*Math.PI)*r+x
-        ,-Math.sin((54+i*72-rota)/180*Math.PI)*r+y);
-    }
-    cxt.closePath(); 
-    
-    cxt.fillStyle="yellow";
-    cxt.strokeStyle="yellow";
-    cxt.lineWidth=3;
-    cxt.lineJoin="round";
-    cxt.fill();
-    cxt.stroke(); 
-    cxt.restore();
-//draw moon
-drawMoon(cxt,600,100,40,3,30,'#FFF68F');
-    
-function drawMoon(cxt,x,y,r,d,rot,fillColor){
+
      //draw a house
      cxt.save();
      cxt.beginPath();
@@ -103,18 +73,8 @@ function drawMoon(cxt,x,y,r,d,rot,fillColor){
      cxt.arc(0,0,1,0.5*Math.PI,1.5*Math.PI,true);
      cxt.moveTo(0,-1);
      cxt.arcTo(d,0,0,1,Math.sqrt(1+d*d)/d);
- }    
-//draw ground
-cxt.save();
-cxt.beginPath();
-cxt.moveTo(0,400);
-cxt.bezierCurveTo(200,300,500,500,800,450);
-cxt.lineTo(800,600);
-cxt.lineTo(0,800);
-cxt.closePath();
-cxt.fillStyle ="green";
-cxt.fill(); 
-     
+ }
+ 
  function drawStar(cxt,x,y,R,fillColor){
      cxt.save();
      cxt.translate(x,y);
