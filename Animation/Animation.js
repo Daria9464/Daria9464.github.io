474997c109bbd37d32ee165cc900fa019b8ce648
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-10-14 23:29:37 -0400

    Update Animation.js

--- Animation/Animation.js
+++ Animation/Animation.js
@@ -1,50 +1,14 @@
-var spritesheet_girl = {
-    "frames": [
-          {
-              "frame": {
-                  "x": 1,
-                  "y": 1,
-                  "w": 567,
-                  "h": 567
-              },
-              "rotated": false,
-              "trimmed": false,
-              "spriteSourceSize": {
-                  "x": 0,
-                  "y": 0,
-                  "w": 567,
-                  "h": 567
-              },
-              "sourceSize": {
-                  "w": 567,
-                  "h": 567
-              }
-          },
-      {
-              "frame": {
-                  "x": 570,
-                  "y": 1,
-                  "w": 567,
-                  "h": 567
-              },
-              "rotated": false,
-              "trimmed": false,
-              "spriteSourceSize": {
-                  "x": 0,
-                  "y": 0,
-                  "w": 567,
-                  "h": 567
-              },
-              "sourceSize": {
-                  "w": 567,
-                  "h": 567
-              }
-          }
-      ]
-  }
-  
-var girlImage = new Image();
-girlImage.src = './spritesheet_girl.png';
+"use strict";
+
+// images
+//var girlImage = new Image();
+//girlImage.src = './spritesheet_girl.png';
+var girlImage = new Array();
+for(var i=0; i<4; i++){
+    girlImage[i]=new Image();
+    var str="./spritesheet_girl"+(i+1)+".png";
+    girlImage[i].src=str;
+}
 
 var dialogBoxImage = new Image();
 dialogBoxImage.src = './dialogBox.png';
@@ -52,41 +16,15 @@ dialogBoxImage.src = './dialogBox.png';
 var weatherImage = new Image();
 weatherImage.src = './spritesheet_sunToWindy.png';
 
-var canvas_count = document.getElementById('canvas');
-var ctx = canvas_count.getContext('2d');
-
-let button = document.querySelector("button");
-
-button.onclick = function(){
-     drawImg();
-     button.style.visibility = "hidden";
- }
-
- function drawNum(i){
-  ctx.drawImage(girlImage,spritesheet_girl.png.frames[i].frame.x,spritesheet_girl.png.frames[i].frame.y, spritesheet_girl.png.frames[i].frame.w, spritesheet_girl.png.frames[i].frame.h, 300, 200, spritesheet_girl.png[i].sourceSize.w, spritesheet_girl.png.frames[i].sourceSize.h);
+window.onload = function(){
+    var cvs = document.getElementById('canvas');
+    var cxt = cvs.getContext('2d');
 }
-
-function drawDialogBox(){
-  ctx.drawImage(dialogBoxImage, 890, 100, 239, 239);
-}
-
-function drawText(){
-  ctx.font = "20px Century Gothic";
-    ctx.fillStyle= "black";
-    ctx.textAlign = "center";
-  ctx.fillText("Keep warm as the weather gets colder", 1010, 200);
-}
-var currentLoop = 0;
-
-function drawImg(){
-    drawDialogBox();
-    drawText();
-     ctx.clearRect(300,200, 567, 567);
-     drawNum(currentLoop);
-     currentLoop++;
-     if (currentLoop == 2){
-       currentLoop = 0;
-    }
-    setTimeout(drawImg, 500);
-
-}
+var i=0;
+var timer=setInterval(function()){
+    cxt.drawImage(girlImage[i],0,0);
+    if(i<3)
+    i++;
+    else
+    i=0;
+},100);
