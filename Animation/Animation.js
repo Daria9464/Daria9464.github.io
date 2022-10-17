53a70fe2767a26532307c4e669b0e15488b9ef80
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-10-17 16:09:01 -0400

    Update Animation.js

--- Animation/Animation.js
+++ Animation/Animation.js
@@ -21,9 +21,40 @@ const height = 18;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * width, frameY * height, width, height,
+                canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
 function init() {
-  ctx.drawImage(girlImage, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
-  ctx.drawImage(girlImage, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
-  ctx.drawImage(girlImage, width * 2, 0, width, height, scaledWidth * 2, 0, scaledWidth, scaledHeight);
+  drawFrame(0, 0, 0, 0);
+  drawFrame(1, 0, scaledWidth, 0);
+  drawFrame(0, 0, scaledWidth * 2, 0);
+  drawFrame(2, 0, scaledWidth * 3, 0);
+}
+
+const cycleLoop = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step() {
+  frameCount++;
+  if (frameCount < 15) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+
+function init() {
+  window.requestAnimationFrame(step);
 }
 
