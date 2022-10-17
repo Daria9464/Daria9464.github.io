0b3c02e2c46f6fcbdd943cc9c59502fd92a4ea54
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-10-17 16:02:25 -0400

    Update Animation.js

--- Animation/Animation.js
+++ Animation/Animation.js
@@ -1,17 +1,16 @@
 "use strict";
 
 // images
-var girlImage = new Image();
+let girlImage = new Image();
 girlImage.src = './spritesheet_girl.png';
 
-var dialogBoxImage = new Image();
+let dialogBoxImage = new Image();
 dialogBoxImage.src = './dialogBox.png';
-
-var weatherImage = new Image();
+let weatherImage = new Image();
 weatherImage.src = './spritesheet_sunToWindy.png';
 
-var cvs = document.getElementById('canvas');
-var cxt = cvs.getContext('2d');
+let cvs = document.getElementById('canvas');
+let cxt = cvs.getContext('2d');
 
 img.onload = function () {
 	init();
@@ -23,8 +22,8 @@ const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 
 function init() {
-  ctx.drawImage(img, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
-  ctx.drawImage(img, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
-  ctx.drawImage(img, width * 2, 0, width, height, scaledWidth * 2, 0, scaledWidth, scaledHeight);
+  ctx.drawImage(girlImage, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
+  ctx.drawImage(girlImage, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
+  ctx.drawImage(girlImage, width * 2, 0, width, height, scaledWidth * 2, 0, scaledWidth, scaledHeight);
 }
 
