91c8be0c27aa1ec961ba2855e8b278c06c0d00f3
Daria9464 <112591776+Daria9464@users.noreply.github.com>
2022-10-27 15:45:59 -0400

    Update TextForm.js

--- TextForm/TextForm.js
+++ TextForm/TextForm.js
@@ -1,16 +1,17 @@
- const txtOutput = document.getElementById("txtOutput");
+        const txtOutput = document.getElementById("txtOutput");
         //function for displaying name
          function textcontent(){
                 var txtName = document.getElementById("txtName");      
                 var name = txtName.value;
                 txtOutput.value = name;
                 
-  }
+        }
 
         //function for changing background color
-        function bgclr(element) {
-               txtOutput.style.backgroundColor = element.value;
-  }
+        function bgclr() {
+               var bgcolor=document.getElementById('bgcolorpicker');
+               txtOutput.style.backgroundColor = bgcolor.value;
+        }
   
 
          //Function for changing text color
@@ -43,11 +44,21 @@
            txtOutput.style.backgroundColor ="White";
            txtOutput.style.fontSize="12px";
            txtOutput.style.color="Black";
-           //the slider also go back to default value 12
+        
+           //input  value go back to empty
+           var txtName = document.getElementById("txtName"); 
+           txtName.value="";
+        
+           //Background colorpicker go back to black
+           var bgcolor=document.getElementById('bgcolorpicker');
+           bgcolor.value="Black";
+        
+           //Font colorpicker go back to black
+           var textcolor=document.getElementById('textcolorpicker');
+            textcolor.value="Black";
+        
+           //the slider go back to default value 12
            var slider_default = "12";
            size.value=slider_default;
            sizedisplay.textContent =size.value;
-           //input  value also go back to empty
-           var txtName = document.getElementById("txtName"); 
-           txtName.value="";
        }
